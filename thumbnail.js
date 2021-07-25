ab28010740eb1db7084f14c7c2652a8415faa618
A-little-bit-Sir <58393906+A-little-bit-Sir@users.noreply.github.com>
2021-07-25 22:26:33 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         if (targetImage.classList.contains("small")) {
              targetImage.classList.remove("small");
        } else {
+           
          targetImage.classList.add("small");
 }
 });
